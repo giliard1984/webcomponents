e9789c36b888a641745e7d90888a73df913649db
Giliard Alves Gomes <giliardalvesgomes@Giliards-MacBook-Pro-2.local>
2024-07-19 10:56:05 +0100

    Added framer motion and a simple example

--- react-webcomponents/src/components/TodoItems/TodoItems.tsx
+++ react-webcomponents/src/components/TodoItems/TodoItems.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
 // components
 import TodoItem from "../TodoItem/TodoItem";
@@ -15,7 +16,30 @@ const TodoItems: React.FC<Props> = ({ data, updateTodoList }) => {
     setTodoItems(data);
   }, [data]);
 
-  if (todoItems?.length) return todoItems.map((todo: Todo) => <TodoItem data={todo} updateTodoList={updateTodoList} />);
+  if (todoItems?.length) {
+    return todoItems.map((todo: Todo, index: number) => 
+      <motion.div
+        key={`todo-item-wrapper-${todo.id}`}
+        initial={{
+          x: -20,
+          y: 0,
+          opacity: 0 
+        }}
+        animate={{
+          x: 0,
+          y: 0,
+          opacity: 1
+        }}
+        transition={{
+          delay: index * 0.1,
+          duration: 0.15,
+          ease: "easeIn"
+        }}
+      >
+        <TodoItem data={todo} updateTodoList={updateTodoList} />
+      </motion.div>
+    );
+  }
   
   return <>Loading...</>;
 }
